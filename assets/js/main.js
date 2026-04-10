fb1e90cfd460342c1af8303ae2b04b30e95c0517
LolosharaGd <lolosharacarrot@gmail.com>
2026-04-11 01:53:57 +0300

    Added bns page saving calc page

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,3 +71,99 @@ function removePriceRow() {
     document.getElementById("price-for-items").removeChild(priOutputs[priOutputs.length - 1]);
     document.getElementById("use-items").removeChild(usiInputs[usiInputs.length - 1]);
 }
+
+function addBNSRow() {
+    var newNckCell = document.createElement("input");
+    newNckCell.type = "text";
+    newNckCell.className = "input-cell-nck";
+    newNckCell.value = "";
+
+    var newScaCell = document.createElement("input");
+    newScaCell.type = "number";
+    newScaCell.className = "input-cell-sca";
+    newScaCell.value = "";
+
+    var newSctCell = document.createElement("input");
+    newSctCell.type = "number";
+    newSctCell.className = "input-cell-sct";
+    newSctCell.value = "";
+    
+    var newTruCell = document.createElement("input");
+    newTruCell.type = "number";
+    newTruCell.className = "input-cell-tru";
+    newTruCell.value = "";
+
+    var newNtsCell = document.createElement("input");
+    newNtsCell.type = "text";
+    newNtsCell.className = "input-cell-nts";
+    newNtsCell.value = "";
+
+    var lastNck = document.getElementById("last-nck");
+    var lastSca = document.getElementById("last-sca");
+    var lastSct = document.getElementById("last-sct");
+    var lastTru = document.getElementById("last-tru");
+    var lastNts = document.getElementById("last-nts");
+    
+    document.getElementById("nicknames").insertBefore(newNckCell, lastNck);
+    document.getElementById("scam-attempts").insertBefore(newScaCell, lastSca);
+    document.getElementById("succesful-trades").insertBefore(newSctCell, lastSct);
+    document.getElementById("trust").insertBefore(newTruCell, lastTru);
+    document.getElementById("notes").insertBefore(newNtsCell, lastNts);
+}
+
+function removeBNSRow() {
+    var nckInputs = document.getElementsByClassName("input-cell-nck");
+    var scaInputs = document.getElementsByClassName("input-cell-sca");
+    var sctInputs = document.getElementsByClassName("input-cell-sct");
+    var truInputs = document.getElementsByClassName("input-cell-tru");
+    var ntsInputs = document.getElementsByClassName("input-cell-nts");
+
+    document.getElementById("nicknames").removeChild(nckInputs[nckInputs.length - 1]);
+    document.getElementById("scam-attempts").removeChild(scaInputs[scaInputs.length - 1]);
+    document.getElementById("succesful-trades").removeChild(sctInputs[sctInputs.length - 1]);
+    document.getElementById("trust").removeChild(truInputs[truInputs.length - 1]);
+    document.getElementById("notes").removeChild(ntsInputs[ntsInputs.length - 1]);
+}
+
+function globalSaveCalcPage() {
+    var itnInputs = document.getElementsByClassName("input-cell-itn");
+    var cppInputs = document.getElementsByClassName("input-cell-cpp");
+    var amtInputs = document.getElementsByClassName("input-cell-amt");
+    var usiInputs = document.getElementsByClassName("input-cell-usi");
+
+    var minElementsCount = Math.min(itnInputs.length, cppInputs.length, amtInputs.length, usiInputs.length);
+
+    var itnItems = [];
+    var cppItems = [];
+    var amtItems = [];
+    var usiItems = [];
+
+    for(var i = 0; i < minElementsCount; i++) {
+        itnItems.push(itnInputs[i].value);
+        cppItems.push(cppInputs[i].value);
+        amtItems.push(amtInputs[i].value);
+        usiItems.push(usiInputs[i].checked);
+    }
+
+    var calcPageData = { len: minElementsCount, itn: itnItems, cpp: cppItems, amt: amtItems, usi: usiItems };
+
+    console.log(calcPageData);
+    localStorage.setItem("calc", JSON.stringify(calcPageData));
+}
+
+function globalLoadCalcPage() {
+    var calcPageData = JSON.parse(localStorage.getItem("calc"));
+
+    console.log(calcPageData);
+
+    for(var i = 0; i < calcPageData.len; i++) {
+        addPriceRow();
+
+        document.getElementsByClassName("input-cell-itn")[i].value = calcPageData.itn[i]
+        document.getElementsByClassName("input-cell-cpp")[i].value = calcPageData.cpp[i]
+        document.getElementsByClassName("input-cell-amt")[i].value = calcPageData.amt[i]
+        document.getElementsByClassName("input-cell-usi")[i].checked = Boolean(calcPageData.usi[i]);
+    }
+
+    removePriceRow();
+}
